d9bcf61da28db3219e0f0e7248fcb3f2a223e68a
Renan O Henrique <henriquebasshvf@gmail.com>
2023-07-31 19:37:22 -0300

    feat: loading fonts on initialize apps

--- App.tsx
+++ App.tsx
@@ -1,15 +1,49 @@
 import * as React from 'react';
 import {Provider} from 'react-redux';
+import {ThemeProvider} from '~/styles';
 import {NavigationContainer} from '@react-navigation/native';
 import store from './src/redux/store';
 import Routes from './src/routes/Routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <Provider store={store}>
-      <Routes />
-    </Provider>
-  </NavigationContainer>
-);
+import {
+  useFonts,
+  Poppins_100Thin,
+  Poppins_200ExtraLight,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+  Poppins_900Black,
+} from '@expo-google-fonts/poppins';
+
+const App: React.FC = () => {
+  let [fontsLoaded] = useFonts({
+    Poppins_100Thin,
+    Poppins_200ExtraLight,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    Poppins_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return <></>;
+  }
+
+  return (
+    <NavigationContainer>
+      <ThemeProvider>
+        <Provider store={store}>
+          <Routes />
+        </Provider>
+      </ThemeProvider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
