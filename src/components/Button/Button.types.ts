7a7a8d9d474cb0539aea84d0fa1e611fd08c7e17
Renan O Henrique <henriquebasshvf@gmail.com>
2023-08-06 11:03:38 -0300

    fix: add option to button show loading

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -5,6 +5,7 @@ type IButtonVariants = 'primary' | 'success' | 'disable' | 'link';
 
 export namespace IButton {
   export interface IView extends IWrappedSystem {
+    isLoading?: boolean;
     variant: IButtonVariants;
     keyLabel: keyof typeof buttons;
   }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
+import {ActivityIndicator} from 'react-native';
 import {Trans} from 'react-i18next';
 import {Div} from '~/components/Atomics';
 import {IButton} from '~/components/Button/Button.types';
 import S from '~/components/Button/Button.styles';
 
-export const Button = ({onPress, ...rest}: IButton.IView) => (
+export const Button = ({
+  onPress,
+  isLoading = false,
+  ...rest
+}: IButton.IView) => (
   <Div onPress={onPress} testID={`Button`}>
     <S.Container {...rest} center height={55} radius={'button'}>
       <S.Label variant={rest.variant}>
-        <Trans i18nKey={`components.Button.${rest.keyLabel}`} />
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <Trans i18nKey={`components.Button.${rest.keyLabel}`} />
+        )}
       </S.Label>
     </S.Container>
   </Div>
